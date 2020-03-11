572fc65bd606938da03e83c81afc23225dd9713d
Sife <sss>
2020-03-12 00:53:26 +0800

    111

---
+++ src/main/frontsource/src/App.vue
@@ -0,0 +1,27 @@
+
+<template>
+  <el-carousel :interval="5000" arrow="always">
+    <el-carousel-item v-for="item in 4" :key="item">
+      <img src="../images/t2.png">
+    </el-carousel-item>
+  </el-carousel>
+</template>
+
+<style>
+  .el-carousel {
+    width: 1320px;
+    height: 820px;
+    margin: 0 auto;
+  }
+  .el-carousel__item {
+    height: 820px;
+  }
+
+  .el-carousel__item button {
+    margin-top: 500px;
+  }
+
+  .el-carousel__item:nth-child(n) {
+    
+  }
+</style>
--- src/main/java/com/sife/sun/SunApplication.java
+++ src/main/java/com/sife/sun/SunApplication.java
@@ -2,7 +2,14 @@ package com.sife.sun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@RestController
 @SpringBootApplication
 public class SunApplication {
 
@@ -10,4 +17,8 @@ public class SunApplication {
         SpringApplication.run(SunApplication.class, args);
     }
 
+    @RequestMapping("/")
+    public String sayHello() {
+        return "hello";
+    }
 }
